172a3d645cae81b92a52bbbfd05b34fa9fc93f0a
gsganden <gsganden@gmail.com>
2021-01-13 08:58:32 -0600

    Loosen restriction on transformers version, add a comment

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'scikit-learn',
         'torch',
         'torchvision==0.2.1',
-        'transformers>=2.3.0,<3.0.0',
+        'transformers>=2.3.0,<4.0.0',  # V4 makes breaking changes
         'wildebeest',
     ],
 )
