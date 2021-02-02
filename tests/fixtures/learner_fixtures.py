e6aafde901ca7598b506c331c17212ee4c02c456
sugi-chan <michaelsugimura@gmail.com>
2021-02-02 15:41:12 -0600

    all the tests

--- tests/fixtures/learner_fixtures.py
+++ tests/fixtures/learner_fixtures.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import pytest
 from torch.utils.data import DataLoader
+from transformers import BertTokenizer
 
 from octopod import MultiDatasetLoader
-from octopod.vision.dataset import OctopodImageDataset
-
+from octopod.text.dataset import OctopodTextDataset, OctopodTextDatasetMultiLabel
+from octopod.vision.dataset import OctopodImageDataset, OctopodImageDatasetMultiLabel
+from octopod.ensemble import OctopodEnsembleDataset, OctopodEnsembleDatasetMultiLabel
 
 @pytest.fixture()
 def test_no_loss_dict():
@@ -89,3 +91,225 @@ def test_train_val_loaders():
     dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
 
     return dataset_loader
+
+@pytest.fixture()
+def mixed_labels():
+    df_string = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'category': ['cat', 'dog']})
+    df_int = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'category': [1, 2]})
+
+    task1_dataset = OctopodImageDataset(
+        x=df_string['image_loc'],
+        y=df_string['category'],
+        transform='train',
+        crop_transform='train'
+    )
+
+    task2_datset = OctopodImageDataset(
+        x=df_int['image_loc'],
+        y=df_int['category'],
+        transform='train',
+        crop_transform='train'
+    )
+
+    dataloaders_dict = {
+        'task1': DataLoader(task1_dataset, batch_size=1, shuffle=True, num_workers=2),
+        'task2': DataLoader(task2_datset, batch_size=1, shuffle=True, num_workers=2),
+    }
+
+    dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
+    dataset_loader2 = MultiDatasetLoader(loader_dict=dataloaders_dict)
+
+    return dataset_loader, dataset_loader2
+
+@pytest.fixture()
+def multi_label_mixed_labels():
+    df_string = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'category': [['cat', 'dog'], ['dog']]})
+    df_int = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'category': [[1, 2], [2]]})
+
+    task1_dataset = OctopodImageDatasetMultiLabel(
+        x=df_string['image_loc'],
+        y=df_string['category'],
+        transform='train',
+        crop_transform='train'
+    )
+
+    task2_datset = OctopodImageDatasetMultiLabel(
+        x=df_int['image_loc'],
+        y=df_int['category'],
+        transform='train',
+        crop_transform='train'
+    )
+
+    dataloaders_dict = {
+        'task1': DataLoader(task1_dataset, batch_size=1, shuffle=True, num_workers=2),
+        'task2': DataLoader(task2_datset, batch_size=1, shuffle=True, num_workers=2),
+    }
+
+    dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
+    dataset_loader2 = MultiDatasetLoader(loader_dict=dataloaders_dict)
+
+    return dataset_loader, dataset_loader2
+
+@pytest.fixture()
+def mixed_labels_text():
+    df_string = pd.DataFrame({'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'category': ['cat', 'dog']})
+    df_int = pd.DataFrame({'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'category': [1, 2]})
+
+    max_seq_length = 128
+    bert_tok = BertTokenizer.from_pretrained(
+    'bert-base-uncased', do_lower_case=True)
+
+    task1_dataset = OctopodTextDataset(
+        x=df_string['fake_text'],
+        y=df_string['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length
+    )
+
+    task2_datset = OctopodTextDataset(
+        x=df_int['fake_text'],
+        y=df_int['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length
+
+    )
+
+    dataloaders_dict = {
+        'task1': DataLoader(task1_dataset, batch_size=1, shuffle=True, num_workers=2),
+        'task2': DataLoader(task2_datset, batch_size=1, shuffle=True, num_workers=2),
+    }
+
+    dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
+    dataset_loader2 = MultiDatasetLoader(loader_dict=dataloaders_dict)
+
+    return dataset_loader, dataset_loader2
+
+@pytest.fixture()
+def multi_label_mixed_labels_text():
+    df_string = pd.DataFrame({'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'category': [['cat', 'dog'], ['dog']]})
+    df_int = pd.DataFrame({'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'category': [[1, 2], [2]]})
+
+    max_seq_length = 128
+    bert_tok = BertTokenizer.from_pretrained('bert-base-uncased',
+                                             do_lower_case=True)
+
+    task1_dataset = OctopodTextDatasetMultiLabel(
+        x=df_string['fake_text'],
+        y=df_string['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length
+    )
+
+    task2_datset = OctopodTextDatasetMultiLabel(
+        x=df_int['fake_text'],
+        y=df_int['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length
+
+    )
+
+    dataloaders_dict = {
+        'task1': DataLoader(task1_dataset, batch_size=1, shuffle=True, num_workers=2),
+        'task2': DataLoader(task2_datset, batch_size=1, shuffle=True, num_workers=2),
+    }
+
+    dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
+    dataset_loader2 = MultiDatasetLoader(loader_dict=dataloaders_dict)
+
+    return dataset_loader, dataset_loader2
+
+@pytest.fixture()
+def mixed_labels_ensemble():
+    df_string = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'category': ['cat', 'dog']})
+    df_int = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'category': [1, 2]})
+
+    max_seq_length = 128
+    bert_tok = BertTokenizer.from_pretrained(
+    'bert-base-uncased', do_lower_case=True)
+
+    task1_dataset = OctopodEnsembleDataset(
+        text_inputs=df_string['fake_text'],
+        img_inputs=df_string['image_loc'],
+        y=df_string['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length,
+        transform='train',
+        crop_transform='train'
+    )
+
+    task2_datset = OctopodEnsembleDataset(
+        text_inputs=df_string['fake_text'],
+        img_inputs=df_string['image_loc'],
+        y=df_int['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length,
+        transform='train',
+        crop_transform='train'
+
+    )
+
+    dataloaders_dict = {
+        'task1': DataLoader(task1_dataset, batch_size=1, shuffle=True, num_workers=2),
+        'task2': DataLoader(task2_datset, batch_size=1, shuffle=True, num_workers=2),
+    }
+
+    dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
+    dataset_loader2 = MultiDatasetLoader(loader_dict=dataloaders_dict)
+
+    return dataset_loader, dataset_loader2
+
+@pytest.fixture()
+def mixed_labels_ensemble():
+    df_string = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                              'category': [['cat', 'dog'], ['dog']]})
+    df_int = pd.DataFrame({'image_loc': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'fake_text': ['sample_data/tonks.jpeg', 'sample_data/tonks.jpeg'],
+                           'category': [[1, 2], [2]]})
+
+    max_seq_length = 128
+    bert_tok = BertTokenizer.from_pretrained(
+    'bert-base-uncased', do_lower_case=True)
+
+    task1_dataset = OctopodEnsembleDatasetMultiLabel(
+        text_inputs=df_string['fake_text'],
+        img_inputs=df_string['image_loc'],
+        y=df_string['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length,
+        transform='train',
+        crop_transform='train'
+    )
+
+    task2_datset = OctopodEnsembleDatasetMultiLabel(
+        text_inputs=df_string['fake_text'],
+        img_inputs=df_string['image_loc'],
+        y=df_int['category'],
+        tokenizer=bert_tok,
+        max_seq_length=max_seq_length,
+        transform='train',
+        crop_transform='train'
+
+    )
+
+    dataloaders_dict = {
+        'task1': DataLoader(task1_dataset, batch_size=1, shuffle=True, num_workers=2),
+        'task2': DataLoader(task2_datset, batch_size=1, shuffle=True, num_workers=2),
+    }
+
+    dataset_loader = MultiDatasetLoader(loader_dict=dataloaders_dict)
+    dataset_loader2 = MultiDatasetLoader(loader_dict=dataloaders_dict)
+
+    return dataset_loader, dataset_loader2
--- tests/test_learner.py
+++ tests/test_learner.py
@@ -1,6 +1,8 @@
 import pytest
 
 from octopod import MultiInputMultiTaskLearner
+from octopod.ensemble import BertResnetEnsembleForMultiTaskClassification
+from octopod.text.models.multi_task_bert import BertForMultiTaskClassification
 from octopod.vision.models import ResnetForMultiTaskClassification
 
 
@@ -140,3 +142,285 @@ def test_learner_with_multi_class_and_label_acc(test_acc_dict_all_tasks_diff_los
 
     assert 'multi_class_accuracy' in str(learn.metric_function_dict['task1'])
     assert 'multi_label_accuracy' in str(learn.metric_function_dict['task2'])
+
+
+def test_learner_w_string_and_int_label_datasets_success_image(mixed_labels):
+    train_dataset_loader, val_dataset_loader = mixed_labels
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = ResnetForMultiTaskClassification(
+        new_task_dict=task_dict)
+
+    learn = MultiInputMultiTaskLearner(model,
+                                       train_dataset_loader,
+                                       val_dataset_loader,
+                                       task_dict)
+
+    assert learn.train_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.train_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+    assert learn.val_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.val_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+
+def test_learner_w_string_datasets_not_matching_categoies_image(mixed_labels):
+    train_dataset_loader, val_dataset_loader = mixed_labels
+    val_dataset_loader.label_mappings['task1'] = {0: 'blue', 3: 'green'}
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = ResnetForMultiTaskClassification(
+        new_task_dict=task_dict)
+
+    with pytest.raises(Exception) as e:
+        MultiInputMultiTaskLearner(model,
+                                   train_dataset_loader,
+                                   val_dataset_loader,
+                                   task_dict)
+    assert ('Mapping mismatch in task1 task. Check that all categories' in str(e.value)) is True
+
+
+def test_learner_w_multilabel_string_and_int_label_datasets_image(multi_label_mixed_labels):
+    train_dataset_loader, val_dataset_loader = multi_label_mixed_labels
+
+    task_dict = {'task1': 2, 'task2': 2}
+    loss_dict = {'task1': 'bce_logits',
+                 'task2': 'bce_logits'}
+    acc_dict = {'task1': 'multi_label_acc',
+                'task2': 'multi_label_acc'}
+    model = ResnetForMultiTaskClassification(
+        new_task_dict=task_dict)
+    learn = MultiInputMultiTaskLearner(model,
+                                       train_dataset_loader,
+                                       val_dataset_loader,
+                                       task_dict,
+                                       loss_function_dict=loss_dict,
+                                       metric_function_dict=acc_dict)
+
+    assert learn.train_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.train_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+    assert learn.val_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.val_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+
+def test_learner_w_string_multilabel_datasets_not_matching_categories_multilabel_image(multi_label_mixed_labels):
+    train_dataset_loader, val_dataset_loader = multi_label_mixed_labels
+    val_dataset_loader.label_mappings['task1'] = {0: 'blue', 3: 'green','wuit':'wiut'}
+
+    task_dict = {'task1': 2, 'task2': 2}
+    loss_dict = {'task1': 'bce_logits',
+                 'task2': 'bce_logits'}
+    acc_dict = {'task1': 'multi_label_acc',
+                'task2': 'multi_label_acc'}
+
+    model = ResnetForMultiTaskClassification(
+        new_task_dict=task_dict)
+
+    with pytest.raises(Exception) as e:
+        MultiInputMultiTaskLearner(model,
+                                   train_dataset_loader,
+                                   val_dataset_loader,
+                                   task_dict,
+                                   loss_function_dict=loss_dict,
+                                   metric_function_dict=acc_dict)
+    assert ('Mapping mismatch in task1 task. Check that all categories' in str(e.value)) is True
+
+
+
+def test_learner_w_string_and_int_label_datasets_success_text(mixed_labels_text):
+    train_dataset_loader, val_dataset_loader = mixed_labels_text
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = BertForMultiTaskClassification.from_pretrained('bert-base-uncased',
+                                                           new_task_dict=task_dict)
+
+    learn = MultiInputMultiTaskLearner(model,
+                                       train_dataset_loader,
+                                       val_dataset_loader,
+                                       task_dict)
+
+    assert learn.train_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.train_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+    assert learn.val_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.val_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+
+def test_learner_w_string_datasets_not_matching_categoies_text(mixed_labels_text):
+    train_dataset_loader, val_dataset_loader = mixed_labels_text
+    val_dataset_loader.label_mappings['task1'] = {0: 'blue', 3: 'green'}
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = BertForMultiTaskClassification.from_pretrained('bert-base-uncased',
+                                                           new_task_dict=task_dict)
+
+    with pytest.raises(Exception) as e:
+        MultiInputMultiTaskLearner(model,
+                                   train_dataset_loader,
+                                   val_dataset_loader,
+                                   task_dict)
+    assert ('Mapping mismatch in task1 task. Check that all categories' in str(e.value)) is True
+
+
+def test_learner_w_multilabel_string_and_int_label_datasets_text(multi_label_mixed_labels_text):
+    train_dataset_loader, val_dataset_loader = multi_label_mixed_labels_text
+
+    task_dict = {'task1': 2, 'task2': 2}
+    loss_dict = {'task1': 'bce_logits',
+                 'task2': 'bce_logits'}
+    acc_dict = {'task1': 'multi_label_acc',
+                'task2': 'multi_label_acc'}
+    model = ResnetForMultiTaskClassification(
+        new_task_dict=task_dict)
+    learn = MultiInputMultiTaskLearner(model,
+                                       train_dataset_loader,
+                                       val_dataset_loader,
+                                       task_dict,
+                                       loss_function_dict=loss_dict,
+                                       metric_function_dict=acc_dict)
+
+    assert learn.train_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.train_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+    assert learn.val_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.val_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+
+def test_learner_w_string_multilabel_datasets_not_matching_categories_multilabel_text(multi_label_mixed_labels_text):
+    train_dataset_loader, val_dataset_loader = multi_label_mixed_labels_text
+    val_dataset_loader.label_mappings['task1'] = {0: 'blue', 3: 'green'}
+
+    task_dict = {'task1': 2, 'task2': 2}
+    loss_dict = {'task1': 'bce_logits',
+                 'task2': 'bce_logits'}
+    acc_dict = {'task1': 'multi_label_acc',
+                'task2': 'multi_label_acc'}
+
+    model = ResnetForMultiTaskClassification(
+        new_task_dict=task_dict)
+
+    with pytest.raises(Exception) as e:
+        MultiInputMultiTaskLearner(model,
+                                   train_dataset_loader,
+                                   val_dataset_loader,
+                                   task_dict,
+                                   loss_function_dict=loss_dict,
+                                   metric_function_dict=acc_dict)
+    assert ('Mapping mismatch in task1 task. Check that all categories' in str(e.value)) is True
+
+
+#### Ensemble
+
+def test_learner_w_string_and_int_label_datasets_success_ensemble(mixed_labels_ensemble):
+    train_dataset_loader, val_dataset_loader = mixed_labels_ensemble
+
+    image_task_dict = {
+        'tasks': {
+            'task1': 2,
+            'task2': 2
+        }  
+    }
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = BertResnetEnsembleForMultiTaskClassification(image_task_dict=image_task_dict)
+
+    learn = MultiInputMultiTaskLearner(model,
+                                       train_dataset_loader,
+                                       val_dataset_loader,
+                                       task_dict)
+
+    assert learn.train_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.train_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+    assert learn.val_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.val_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+
+def test_learner_w_string_datasets_not_matching_categoies_text(mixed_labels_ensemble):
+    train_dataset_loader, val_dataset_loader = mixed_labels_ensemble
+    val_dataset_loader.label_mappings['task1'] = {0: 'blue', 3: 'green'}
+
+    image_task_dict = {
+        'tasks': {
+            'task1': 2,
+            'task2': 2
+        }  
+    }
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = BertResnetEnsembleForMultiTaskClassification(image_task_dict=image_task_dict)
+
+    with pytest.raises(Exception) as e:
+        MultiInputMultiTaskLearner(model,
+                                   train_dataset_loader,
+                                   val_dataset_loader,
+                                   task_dict)
+    assert ('Mapping mismatch in task1 task. Check that all categories' in str(e.value)) is True
+
+
+def test_learner_w_multilabel_string_and_int_label_datasets_ensemble(multi_label_mixed_labels_text):
+    train_dataset_loader, val_dataset_loader = multi_label_mixed_labels_text
+
+    loss_dict = {'task1': 'bce_logits',
+                 'task2': 'bce_logits'}
+    acc_dict = {'task1': 'multi_label_acc',
+                'task2': 'multi_label_acc'}
+    image_task_dict = {
+        'tasks': {
+            'task1': 2,
+            'task2': 2
+        }
+    }
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = BertResnetEnsembleForMultiTaskClassification(image_task_dict=image_task_dict)
+
+    learn = MultiInputMultiTaskLearner(model,
+                                       train_dataset_loader,
+                                       val_dataset_loader,
+                                       task_dict,
+                                       loss_function_dict=loss_dict,
+                                       metric_function_dict=acc_dict)
+
+    assert learn.train_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.train_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+    assert learn.val_dataloader.label_mappings['task1'] == {0: 'cat', 1: 'dog'}
+    assert learn.val_dataloader.label_mappings['task2'] == {0: 1, 1: 2}
+
+
+def test_learner_w_string_multilabel_datasets_not_matching_categories_multilabel_ensemble(multi_label_mixed_labels_text):
+    train_dataset_loader, val_dataset_loader = multi_label_mixed_labels_text
+    val_dataset_loader.label_mappings['task1'] = {0: 'blue', 3: 'green'}
+
+    loss_dict = {'task1': 'bce_logits',
+                 'task2': 'bce_logits'}
+    acc_dict = {'task1': 'multi_label_acc',
+                'task2': 'multi_label_acc'}
+
+    image_task_dict = {
+        'tasks': {
+            'task1': 2,
+            'task2': 2
+        }
+    }
+
+    task_dict = {'task1': 2, 'task2': 2}
+
+    model = BertResnetEnsembleForMultiTaskClassification(image_task_dict=image_task_dict)
+
+    with pytest.raises(Exception) as e:
+        MultiInputMultiTaskLearner(model,
+                                   train_dataset_loader,
+                                   val_dataset_loader,
+                                   task_dict,
+                                   loss_function_dict=loss_dict,
+                                   metric_function_dict=acc_dict)
+    assert ('Mapping mismatch in task1 task. Check that all categories' in str(e.value)) is True
