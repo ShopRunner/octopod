ad74df9fbf42f886a1ceba48fa0aa68b234b38c7
Sreesh Nair <snair@shoprunner.com>
2022-01-23 00:39:44 +0530

    fixing the batch count

--- octopod/dataloader.py
+++ octopod/dataloader.py
@@ -12,6 +12,7 @@ class MultiDatasetLoader(object):
     shuffle: Boolean (defaults to True)
         Flag for whether or not to shuffle the data
     """
+
     def __init__(self, loader_dict, shuffle=True):
         self.loader_dict = loader_dict
         self.shuffle = shuffle
@@ -28,7 +29,7 @@ class MultiDatasetLoader(object):
         iterators = {}
 
         for key in self.loader_dict.keys():
-            current_batches = [key] * len(self.loader_dict[key])
+            current_batches = [key] * self._get_no_of_batches(key)
             named_batches += current_batches
             iterators[key] = iter(self.loader_dict[key])
 
@@ -36,18 +37,39 @@ class MultiDatasetLoader(object):
             random.shuffle(named_batches)
 
         for key in named_batches:
-            items = next(iterators[key])
-            if self._get_num_rows(items[0]) == 1:
-                continue
-            yield key, items
+            yield key, next(iterators[key])
 
     def __len__(self):
         num_batches = 0
 
         for key in self.loader_dict.keys():
-            num_batches += len(self.loader_dict[key])
+            num_batches += self._get_no_of_batches(key)
 
         return num_batches
+
+    def _get_no_of_batches(self, key, threshold_batch_size=1):
+        """
+        The method ignores the batches that has number of records below the threshold_batch_size
+        Say if the threshold_batch_size is set as 2,
+            and total no of data is 130 and batch size 64, then 66 % 64 <= 2, and would return 130 //64 == 1
+            and if batch size is 134 , then 134 % 64 > 2, then would return len(dataset) == 2
+        Parameters
+        ----------
+        key: str
+            The key for the dictionary
+        threshold_batch_size: Integer
+            threshold size for the batch to be considered for processing
+            default 1
+
+        Return
+        ----------
+            return no of batches those no of records above the threshold_batch_size
+        """
+        total_size = len(self.loader_dict[key].dataset)
+        batch_size = self.loader_dict[key].batch_size
+        if total_size % batch_size <= threshold_batch_size:
+            return total_size // batch_size
+        return len(self.loader_dict[key])
 
     def _gen_label_mappings(self):
 
@@ -55,6 +77,3 @@ class MultiDatasetLoader(object):
         for key, loader_dict in self.loader_dict.items():
             mapping_dict[key] = loader_dict.dataset.label_mapping
         return mapping_dict
-
-    def _get_num_rows(self, x):
-        return x[next(iter(x))].size(0)
