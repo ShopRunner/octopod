a7e8164f4bac53a7069f2c9b9570444d207b0cee
gsganden <gsganden@gmail.com>
2022-02-18 16:07:14 -0600

    Revert unintented format change

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='octopod',
     version=__version__,
     description='General purpose multi-task classification library',
-    long_description=readme + '\n\n\nLicense\n-------\n' + license,
+    long_description=readme+'\n\n\nLicense\n-------\n'+license,
     long_description_content_type='text/markdown',
     author='Nicole Carlson, Michael Sugimura',
     url='https://github.com/shoprunner/octopod',
