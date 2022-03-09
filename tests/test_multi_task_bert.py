67ed0b7f153d7d9ec46d7330acc4d43e04d76b57
Jeremy Lerner <lernerjn@gmail.com>
2022-03-09 16:00:10 -0500

    Update test_multi_task_bert.py

--- tests/test_multi_task_bert.py
+++ tests/test_multi_task_bert.py
@@ -8,36 +8,6 @@ import torch
 from octopod.text.models import BertForMultiTaskClassification
 
 
-def test_exporting_and_loading_works_correctly():
-    task_dict = {'fake_attribute': 10}
-
-    model = BertForMultiTaskClassification.from_pretrained(
-        'bert-base-uncased',
-        new_task_dict=task_dict
-    )
-
-    model_id = 27
-
-    test_dir = Path() / tempfile.mkdtemp()
-
-    model.export(test_dir, model_id)
-
-    new_model = BertForMultiTaskClassification.from_pretrained(
-        'bert-base-uncased',
-        pretrained_task_dict=task_dict
-    )
-
-    new_model.load_state_dict(torch.load(
-        test_dir / f'multi_task_bert_{model_id}.pth',
-        map_location=lambda storage,
-        loc: storage
-    ))
-    shutil.rmtree(test_dir)
-
-    for original_val, new_val in zip(model.state_dict().values(), new_model.state_dict().values()):
-        assert torch.equal(original_val, new_val)
-
-
 def test_exporting_and_importing_works_correctly():
     task_dict = {'fake_attribute': 10}
 
